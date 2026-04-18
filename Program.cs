ce71332de79049f2aa4cc348086acd283f4da2df
Karim <karimgubaidullin6@gmail.com>
2026-04-18 14:48:59 +0300

    Update Program.cs
    I update you code

--- Program.cs
+++ Program.cs
@@ -5,19 +5,19 @@ string profession = "Software Developer";
 string skills = "C#, JavaScript, Linux, Agile.";
 string location = "Johannesburg, South Africa";
 string contact = "faniejohnsons98@gmail.com";
+=======================================
+          Personal Information         
+=======================================
+Name:          {name}
+Age:           {age}
+Profession:    {profession}
+Skills:        {skills}
+Location:      {location}
+Contact:       {contact}
+=======================================
+           Thank you for visiting!     
+=======================================
+";
+Console.WriteLine(a);
 
-Console.WriteLine("=======================================");
-Console.WriteLine("          Personal Information         ");
-Console.WriteLine("=======================================");
-Console.WriteLine();
-Console.WriteLine($"Name:          {name}");
-Console.WriteLine($"Age:           {age}");
-Console.WriteLine($"Profession:    {profession}");
-Console.WriteLine($"Skills:        {skills}");
-Console.WriteLine($"Location:      {location}");
-Console.WriteLine($"Contact:       {contact}");
-Console.WriteLine();
-Console.WriteLine("=======================================");
-Console.WriteLine("           Thank you for visiting!     ");
-Console.WriteLine("=======================================");
     
